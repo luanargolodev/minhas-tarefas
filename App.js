5e147613b6b5fed1d64ef9c15e8a88ad3b4ce222
Luan Argolo <ipsluan@icloud.com>
2022-04-08 00:14:47 -0300

    :sparkles: feat: add application skeleton

--- App.js
+++ App.js
@@ -1,20 +1,169 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  TextInput,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import TaskList from "./src/components/TaskList";
+
+import * as Animatable from "react-native-animatable";
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
 export default function App() {
+  const [task, setTask] = useState([
+    { key: 1, task: "Lembrete" },
+    { key: 2, task: "Lembrete" },
+    { key: 3, task: "Lembrete" },
+    { key: 4, task: "Lembrete" },
+    { key: 5, task: "Lembrete" },
+  ]);
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="171d31" barStyle="light-content" />
+
+      <View>
+        <Text style={styles.title}>Minhas Tarefas</Text>
+      </View>
+
+      <FlatList
+        marginHorizontal={10}
+        showsHorizontalScrollIndicator={false}
+        data={task}
+        keyExtractor={(item) => String(item.key)}
+        renderItem={({ item }) => <TaskList data={item} />}
+      />
+
+      <Modal animationType="slide" transparent={false} visible={open}>
+        <SafeAreaView style={styles.modal}>
+          <View style={styles.modalHeader}>
+            <TouchableOpacity onPress={() => setOpen(false)}>
+              <Ionicons
+                style={{ marginLeft: 5, marginRight: 5 }}
+                name="md-arrow-back"
+                size={40}
+                color="#FFF"
+              />
+            </TouchableOpacity>
+            <Text style={styles.modalTitle}>Nova Tarefa</Text>
+          </View>
+
+          <Animatable.View
+            style={styles.modalBody}
+            animation="fadeInUp"
+            useNativeDriver
+          >
+            <TextInput
+              placeholder="Digite sua tarefa"
+              style={styles.input}
+              multiline={true}
+              placeholderTextColor="#747474"
+              autoCorret={false}
+              value={input}
+              onChangeText={(texto) => setInput(texto)}
+            />
+            <TouchableOpacity style={styles.handleAdd}>
+              <Text style={styles.handleAddText}>Cadastrar</Text>
+            </TouchableOpacity>
+          </Animatable.View>
+        </SafeAreaView>
+      </Modal>
+
+      <AnimatedBtn
+        style={styles.fab}
+        useNativeDriver
+        animation="bounceInUp"
+        duration={1500}
+        onPress={() => setOpen(true)}
+      >
+        <Ionicons name="ios-add" size={32} color="#FFF" />
+      </AnimatedBtn>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#171d31",
+  },
+  title: {
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: "center",
+    color: "#fff",
+  },
+  fab: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    backgroundColor: "#0094ff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    },
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: "#171d31",
+  },
+  modalHeader: {
+    marginLeft: 10,
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 23,
+    color: "#fff",
+    marginLeft: 15,
+  },
+  modalBody: {
+    marginTop: 15,
+  },
+  input: {
+    fontSize: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 30,
+    backgroundColor: "#fff",
+    padding: 9,
+    height: 85,
+    textAlignVertical: "top",
+    color: "#000",
+    borderRadius: 5,
+  },
+  handleAdd: {
+    backgroundColor: "#fff",
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 10,
+    marginRight: 10,
+    height: 40,
+    borderRadius: 5,
+  },
+  handleAddText: {
+    fontSize: 20,
   },
 });
